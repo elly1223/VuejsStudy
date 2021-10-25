c57b90331c013730169de228ff5dce583c332024
이윤경 <yunkyunglee@iyungyeongs-MacBook-Pro.local>
2021-10-25 11:14:52 +0900

    4th

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ var app = new Vue({
       message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
     }
   })
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  })
+
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'JavaScript 배우기' },
+        { text: 'Vue 배우기' },
+        { text: '무언가 멋진 것을 만들기' }
+      ]
+    }
+  })
